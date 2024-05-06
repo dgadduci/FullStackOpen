c450aee0a56d28200d6e37012a0f819972075f4d
Diego Adduci La Greca <diegoadducilagreca@MacBook-Pro-de-Diego-5.local>
2024-05-06 11:36:29 -0300

    completed 1.1-1.5

---
+++ part1/part1.4/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part)=>(
+        <Part key = {part.name} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  let total = 0;
+  parts.forEach((part) => {
+    total = total + part.exercises;
+  });
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/part1.5/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  let total = 0;
+  parts.forEach((part) => {
+    total = total + part.exercises;
+  });
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
