cce3c05230090a9e9de7bc21665f807b3c021354
Diego Adduci La Greca <diegoadducilagreca@MacBook-Pro-de-Diego-5.local>
2024-05-21 15:05:26 -0300

    frontend finish

--- part3/phones/src/App.jsx
+++ part3/phones/src/App.jsx
@@ -11,12 +11,11 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [filterName, setFilterName] = useState("");
-  const[message, setMessage]= useState(null);
+  const [message, setMessage] = useState(null);
 
   //load initial data from server
   useEffect(() => {
-    personService.getAll(handleMessage).
-    then(response => {
+    personService.getAll(handleMessage).then((response) => {
       setPersons(response);
     });
   }, []);
@@ -30,47 +29,51 @@ const App = () => {
     setNewPhone(event.target.value);
   };
 
-  const handleNewPerson = (event) => {
+  const handlesubmit = (event) => {
     event.preventDefault();
-    const person =persons.find((person) => person.name.toLowerCase() === newName.toLocaleLowerCase());
+    const person = persons.find(
+      (person) => person.name.toLowerCase() === newName.toLocaleLowerCase()
+    );
+    //new person
     if (person === undefined) {
-      const newPerson={ name: newName, number: newPhone, id: (persons.length + 1).toString() };
-      setPersons([
-        ...persons,
-        newPerson,
-      ]);
-      setNewName("");
-      setNewPhone("");
-      personService.create(newPerson, handleMessage);
+      const newPerson = { name: newName, number: newPhone };
+      personService.create(newPerson, handleMessage).then((person) => {
+        newPerson.id = person.data.id;
+        setPersons([...persons, newPerson]);
+      });
     } else {
-      //confirm if update person
-      if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
-        //update repository
-        person.number = newPhone;
-        personService.update(person, handleMessage);
-        //update UI
-        setPersons(
-          persons.map((p)=> p.id != person.id ? p : person)
-        );
+      //update person
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const updatePerson = {...person,name:newName, number:newPhone};
+        personService.update(updatePerson, handleMessage, () => {
+          //update UI
+          setPersons(persons.map((p) => (p.id != person.id ? p : updatePerson)));
+        });
       }
     }
+    setNewName("");
+    setNewPhone("");
   };
 
-  const handleDeletePerson =(id)=>{
+  const handleDeletePerson = (id) => {
     personService.remove(id, handleMessage);
-    setPersons( persons.filter(person => person.id != id));
-  }
+    setPersons(persons.filter((person) => person.id != id));
+  };
 
   const handleFilterName = (event) => {
     setFilterName(event.target.value);
   };
 
-  const handleMessage =(message) => {
+  const handleMessage = (message) => {
     setMessage(message);
     setTimeout(() => {
       setMessage(null);
     }, 4000);
-  }
+  };
 
   const personsShow = persons.filter((person) =>
     person.name.toLowerCase().includes(filterName.toLowerCase())
@@ -78,8 +81,8 @@ const App = () => {
 
   return (
     <div>
-      <h1>Phonebook!!</h1>
-      <MessageBar message = {message}/>
+      <h1>Phonebook</h1>
+      <MessageBar message={message} />
       <Filter
         label="filter shown with: "
         value={filterName}
@@ -90,118 +93,13 @@ const App = () => {
         newName={newName}
         newPhone={newPhone}
         handleNameInput={handleNameInput}
-        handleNewPerson={handleNewPerson}
         handlePhoneInput={handlePhoneInput}
+        handleNewPerson={handlesubmit}
       />
       <Title text="Numbers" />
-      <List persons={personsShow} handleDelete={handleDeletePerson}/>
+      <List persons={personsShow} handleDelete={handleDeletePerson} />
     </div>
   );
 };
 
 export default App;
-
-/*
-import { useEffect, useState } from "react";
-import Filter from "./components/Filter";
-import Title from "./components/Title";
-import List from "./components/List";
-import Form from "./components/Form";
-import personService from "./services/persons";
-import MessageBar from "./components/MessageBar";
-
-const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newPhone, setNewPhone] = useState("");
-  const [filterName, setFilterName] = useState("");
-  const[message, setMessage]= useState(null);
-
-  //load initial data from server
-  useEffect(() => {
-    personService.getAll(handleMessage).
-    then(response => {
-      setPersons(response);
-    });
-  }, []);
-
-  //handlers UI components
-  const handleNameInput = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handlePhoneInput = (event) => {
-    setNewPhone(event.target.value);
-  };
-
-  const handleNewPerson = (event) => {
-    event.preventDefault();
-    const person =persons.find((person) => person.name.toLowerCase() === newName.toLocaleLowerCase());
-    if (person === undefined) {
-      const newPerson={ name: newName, number: newPhone, id: (persons.length + 1).toString() };
-      setPersons([
-        ...persons,
-        newPerson,
-      ]);
-      setNewName("");
-      setNewPhone("");
-      personService.create(newPerson, handleMessage);
-    } else {
-      //confirm if update person
-      if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
-        //update repository
-        person.number = newPhone;
-        personService.update(person, handleMessage);
-        //update UI
-        setPersons(
-          persons.map((p)=> p.id != person.id ? p : person)
-        );
-      }
-    }
-  };
-
-  const handleDeletePerson =(id)=>{
-    personService.remove(id, handleMessage);
-    setPersons( persons.filter(person => person.id != id));
-  }
-
-  const handleFilterName = (event) => {
-    setFilterName(event.target.value);
-  };
-
-  const handleMessage =(message) => {
-    setMessage(message);
-    setTimeout(() => {
-      setMessage(null);
-    }, 4000);
-  }
-
-  const personsShow = persons.filter((person) =>
-    person.name.toLowerCase().includes(filterName.toLowerCase())
-  );
-
-  return (
-    <div>
-      <h1>Phonebook!!</h1>
-      <MessageBar message = {message}/>
-      <Filter
-        label="filter shown with: "
-        value={filterName}
-        handle={handleFilterName}
-      />
-      <Title text="add a new" />
-      <Form
-        newName={newName}
-        newPhone={newPhone}
-        handleNameInput={handleNameInput}
-        handleNewPerson={handleNewPerson}
-        handlePhoneInput={handlePhoneInput}
-      />
-      <Title text="Numbers" />
-      <List persons={personsShow} handleDelete={handleDeletePerson}/>
-    </div>
-  );
-};
-
-export default App;
-*/
--- part3/phones/src/services/persons.js
+++ part3/phones/src/services/persons.js
@@ -9,22 +9,23 @@ const getAll = (handleMessage) => {
   return request
     .then((response) => response.data)
     .catch((error) => {
-        handleMessage(MessageError("An error occurred while trying to retrieve the documents from the agenda"))
-      console.log(error);
+      handleMessage(
+        MessageError(
+          "An error occurred while trying to retrieve the documents from the agenda"
+        )
+      );
     });
 };
 
 const create = (newPerson, handleMessage) => {
   const request = axios.post(baseUrl, newPerson);
   return request
-    .then(() => {
+    .then((person) => {
       handleMessage(MessageConfirm(`${newPerson.name} was add to the agenda`));
+      return person;
     })
     .catch((error) => {
-      handleMessage(
-        MessageError(`${newPerson.name} could not be added to the agenda`)
-      );
-      console.log(error);
+      handleMessage(MessageError(error.response.data.error));
     });
 };
 
@@ -32,83 +33,25 @@ const remove = (id, handleMessage) => {
   const request = axios.delete(`${baseUrl}/${id}`);
   request
     .then(() =>
-      handleMessage(
-        MessageConfirm("The person was removed from the agenda")
-      )
+      handleMessage(MessageConfirm("The person was removed from the agenda"))
     )
     .catch((error) => {
-      handleMessage(MessageError("The person could not be removed from the agenda"));
-      console.log(error);
+      handleMessage(
+        MessageError("The person could not be removed from the agenda")
+      );
     });
 };
 
-const update = (person, handleMessage) => {
+const update = (person, handleMessage, onUpdate) => {
   const request = axios.put(`${baseUrl}/${person.id}`, person);
   request
-    .then(() => handleMessage(MessageConfirm(`${person.name} was updated`)))
-    .catch((error) => {
-      handleMessage(MessageError(`${person.name} could not updated`));
-      console.log(error);
-    });
-};
-
-export default { getAll, create, remove, update };
-
-
-/*
-import axios from "axios";
-import MessageError from "../components/messageError";
-import MessageConfirm from "../components/MessageConfirm";
-
-const baseUrl = "/api/persons";
-
-const getAll = (handleMessage) => {
-  const request = axios.get(baseUrl);
-  return request
-    .then((response) => response.data)
-    .catch((error) => {
-        handleMessage(MessageError("An error occurred while trying to retrieve the documents from the agenda"))
-      console.log(error);
-    });
-};
-
-const create = (newPerson, handleMessage) => {
-  const request = axios.post(baseUrl, newPerson);
-  return request
     .then(() => {
-      handleMessage(MessageConfirm(`${newPerson.name} was add to the agenda`));
+      handleMessage(MessageConfirm(`${person.name} was updated`));
+      onUpdate();
     })
     .catch((error) => {
-      handleMessage(
-        MessageError(`${newPerson.name} could not be added to the agenda`)
-      );
-      console.log(error);
-    });
-};
-
-const remove = (id, handleMessage) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  request
-    .then(() =>
-      handleMessage(
-        MessageConfirm("The person was removed from the agenda")
-      )
-    )
-    .catch((error) => {
-      handleMessage(MessageError("The person could not be removed from the agenda"));
-      console.log(error);
-    });
-};
-
-const update = (person, handleMessage) => {
-  const request = axios.put(`${baseUrl}/${person.id}`, person);
-  request
-    .then(() => handleMessage(MessageConfirm(`${person.name} was updated`)))
-    .catch((error) => {
-      handleMessage(MessageError(`${person.name} could not updated`));
-      console.log(error);
+      handleMessage(MessageError(error.response.data.error));
     });
 };
 
 export default { getAll, create, remove, update };
-*/
